433d1684b80ffc0c9f16458b3dcbaa38490c82f5
surgutov55 <43478792+surgutov55@users.noreply.github.com>
2020-10-28 01:50:38 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -8,3 +8,18 @@ window.addEventListener('beforeinstallprompt', (e) => {
   // Update UI notify the user they can install the PWA
   showInstallPromotion();
 });
+
+buttonInstall.addEventListener('click', (e) => {
+  // Hide the app provided install promotion
+  hideMyInstallPromotion();
+  // Show the install prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice.then((choiceResult) => {
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the install prompt');
+    } else {
+      console.log('User dismissed the install prompt');
+    }
+  });
+});
